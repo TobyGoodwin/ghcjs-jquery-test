138cc96e054429882317bd7d6c3324c11536b705
Toby Goodwin <toby@rocinante.mythic-beasts.com>
2015-02-13 19:20:17 +0000

    InType cannot work

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,6 +2,8 @@ module Handler.Home where
 
 import Import
 
+import qualified Data.Map as Map
+
 getHomeR :: Handler Html
 getHomeR = do
   defaultLayout $ do
@@ -11,3 +13,17 @@ getHomeR = do
 
 postPlainR :: Handler TypedContent
 postPlainR = selectRep $ provideRep $ return ("plain ok" :: Text)
+
+postPairsR :: Handler TypedContent
+postPairsR = do
+  (e, f) <- postEnv
+  print e
+  selectRep $ provideRep $ return ("pairs ok" :: Text)
+
+-- from Yesod.Form.Functions
+postEnv :: (MonadHandler m, MonadResource m)
+        => m (Env, FileEnv)
+postEnv = do
+    (p, f) <- runRequestBody
+    let p' = Map.unionsWith (++) $ map (\(x, y) -> Map.singleton x [y]) p
+    return $ (p', Map.unionsWith (++) $ map (\(k, v) -> Map.singleton k [v]) f)
