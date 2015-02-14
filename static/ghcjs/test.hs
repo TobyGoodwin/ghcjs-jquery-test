267124d52ac66d551a2fa396e86f8d96e1b237b9
Toby Goodwin <toby@rocinante.mythic-beasts.com>
2015-02-14 21:45:45 +0000

    test for string pairs

--- static/ghcjs/test.hs
+++ static/ghcjs/test.hs
@@ -5,43 +5,28 @@ module Main where
 
 import ClassyPrelude
 
-import Data.Aeson (ToJSON, toJSON)
 import JavaScript.JQuery
 
-data Test o = Test { name :: Text
-                   , url :: Text
-                   , input :: InType
-                   , rStatus :: Int
-                   , rData :: o
-                   }
-
-data InType = S String | T Text | P [(Text,Text)]
-instance ToAjax InType
-  where toAjax (S x) = toAjax x
-        toAjax (T x) = toAjax x
-        toAjax (P x) = toAjax x
-
-instance ToJSON InType
-  where toJSON (S x) = toJSON x
-        toJSON (T x) = toJSON x
-        toJSON (P x) = toJSON x
-
-tests =
-  [ Test "nonexistent" "nonesuch" (S "some string") 404 Nothing
-  , Test "string" "plain" (S "some string") 200 (Just "plain ok")
-  , Test "text" "plain" (T "some text") 200 (Just "plain ok")
-  , Test "pairs" "pairs"
-      (P [("one","pair"),("another","couple")]) 200 (Just "pairs ok")
-  ]
-
-main = mapM_ runTest tests
+main = do
+  t "nonexistent" "nonesuch" string 404 Nothing
+  t "string" "plain" string 200 (Just "plain ok")
+  t "text" "plain" text 200 (Just "plain ok")
+  t "pairsS" "pairs" pairsS 200 (Just "pairs ok")
+  t "pairsT" "pairs" pairsT 200 (Just "pairs ok")
   where
-    runTest t = do
-      say $ "about to run test " ++ name t
-      r <- ajax3 (url t) (input t)
-      if arStatus r == rStatus t && arData r == rData t
-        then say $ name t ++ " passed"
-        else say $ name t ++ " failed: r is " ++ tshow r
+    t name url input rStatus rData = do
+      say $ "about to run test " ++ name
+      r <- ajax3 url input
+      if arStatus r == rStatus && arData r == rData
+        then say $ name ++ " passed"
+        else say $ name ++ " failed: r is " ++ tshow r
+
+string = "some string" :: String
+text = "some text" :: Text
+pairsS =
+  [("one","pair"),("another","couple"),("and","third")] :: [(String,String)]
+pairsT =
+  [("one","pair"),("another","couple"),("and","third")] :: [(Text,Text)]
 
 say :: Text -> IO ()
 say x = do
